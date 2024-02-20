ef1ca3ee205693618d024ec62a0de5b89dcd142a
Ray Lee <ray.lee@osckorea.com>
2024-02-20 17:15:14 +0900

    Add : 넥서스 저장소 Asset 관련 Enum 작성

--- src/main/java/com/osckorea/osms/nxrm/core/enums/NxrmAssetFileTypeEnum.java
+++ src/main/java/com/osckorea/osms/nxrm/core/enums/NxrmAssetFileTypeEnum.java
@@ -1,5 +1,54 @@
 package com.osckorea.osms.nxrm.core.enums;
 
+import java.util.Collections;
+import java.util.List;
+
 public enum NxrmAssetFileTypeEnum {
+    MAVEN2(List.of(".pom", ".jar", ".zip"), false),
+    NPM(List.of(".tgz"), true), 
+    PYPI(List.of(".tar.gz"), true),
+    APT(List.of(".deb", ".udeb"), true),
+    HELM(List.of(".tgz", ".tgz.prov"), true),
+    NUGET(List.of(".nupkg"), true),
+    RUBYGEMS(List.of(".gem"),true),
+    YUM(List.of(".rpm", ".drpm"), true),
+    R(List.of(".tar.gz", ".tgz"), true),
+    RAW(false),
+    ;
+
+    protected List<String> fileTypes;
+    protected final Boolean restriction;
+
+    NxrmAssetFileTypeEnum(Boolean restrction) {
+        this.restriction = restrction;
+    }
+
+    NxrmAssetFileTypeEnum(List<String> fileTypes, Boolean restrction) {
+        this.fileTypes = fileTypes;
+        this.restriction = restrction;
+    }
+
+    public List<String> getFileTypes() {
+        return this.fileTypes != null ? this.fileTypes : Collections.emptyList();
+    }
+
+    public Boolean getRestriction() {
+        return this.restriction;
+    }
+
+    public Boolean validateFileExtension(String fileName) {
+        if (restriction) {
+            Boolean flag = false;
+            
+            for (String extension : fileTypes) {
+                if (fileName.endsWith(extension)) {
+                    flag = true;
+                }
+            }
     
+            return flag;
+        } else {
+            return true;
+        }
+    };
 }
--- src/main/java/com/osckorea/osms/nxrm/core/enums/NxrmAssetKindEnum.java
+++ src/main/java/com/osckorea/osms/nxrm/core/enums/NxrmAssetKindEnum.java
@@ -1,5 +1,57 @@
 package com.osckorea.osms.nxrm.core.enums;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public enum NxrmAssetKindEnum {
-    
+    MAVEN2("maven2", "ARTIFACT"),
+    PYPI("pypi", "PACKAGE"),
+    NPM("npm", "TARBALL"),
+    R("r", "ARCHIVE"),
+    APT("apt", "DEV"),
+    RUBYGEMS("rubygems", "GEM"),
+    YUM("yum", "RPM"),
+    HELM("helm", "HELM_PACKAGE"),
+    BOWER ("bower", "TARBALL"),
+    DOCKER("docker", "MANIFEST"),
+    GO("go"),
+    NUGET("nuget"),
+    RAW("raw"),
+    CONAN("conan"),
+    COCOAPODS("cocoapods"),
+    ;
+
+    private final String format;
+    private String assetKind;
+
+    NxrmAssetKindEnum(String format) {
+        this.format = format;
+    }
+
+    NxrmAssetKindEnum(String format, String assetKind) {
+        this.format = format;
+        this.assetKind = assetKind;
+    }
+
+    public String getFormat() {
+        return this.format;
+    }
+
+    public String getAssetKind() {
+        return this.assetKind != null ? this.assetKind : "";
+    }
+
+    public static NxrmAssetKindEnum findByFormat(String format) {
+        return Stream.of(values())
+                        .filter(val -> val.getFormat().equals(format))
+                        .findFirst()
+                        .get();
+    }
+
+    public static List<String> findAllFormat() {
+        return Stream.of(values())
+                        .map(val -> val.getFormat())
+                        .collect(Collectors.toList());
+    }
 }
