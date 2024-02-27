6d4173a64a716906499734d97eefdcdb3cf475d1
Ray Lee <ray.lee@osckorea.com>
2024-02-27 10:58:33 +0900

    Add : Builder Annotaion 추가

--- src/main/java/com/osckorea/osms/nxrm/core/dto/pub/tag/TagDto.java
+++ src/main/java/com/osckorea/osms/nxrm/core/dto/pub/tag/TagDto.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Builder
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
